87d634b8be103f759834a30c956917071f0211ea
vitoloiudice-web <vitoloiudice@gmail.com>
2025-10-31 10:30:50 +0100

    Update Profilo Aziendale

--- data.ts
+++ data.ts
@@ -1,10 +1,10 @@
 import type { CompanyProfile } from './types.ts';
 
 export const MOCK_COMPANY_PROFILE: CompanyProfile = {
-    companyName: 'Il Tuo Nome / Nome Attività',
-    vatNumber: '12345678901',
-    address: 'Via di Esempio 1, 00100 Città (XX)',
-    email: 'tua.email@esempio.com',
-    phone: '333 123 4567',
+    companyName: 'ILARIA TAVANI',
+    vatNumber: 'IT 09038130721',
+    address: 'Via Chiancaro 2 N, 70010 Adelfia (BA)',
+    email: 'labeasypeasy@gmail.com',
+    phone: '(+39) 340 523 4353',
     taxRegime: "Operazione effettuata in regime fiscale di vantaggio ai sensi dell'articolo 1, commi 54-89, Legge n. 190/2014 e successive modificazioni. Si richiede di non operare la ritenuta alla fonte a titolo di acconto ai sensi dell'articolo 27, D.P.R. 29 settembre 1973, n. 600."
 };
